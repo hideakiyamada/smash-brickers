c555bcde3fe2bfbbdca4203e902ea2eaffa847db
Hideaki Yamada <hideakiyamada@icloud.com>
2025-06-07 10:11:45 +0900

    build: documentサイトをgithub pagesで公開するためにdocusaurus.config.tsを更新

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -10,15 +10,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://smash-brickers.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/smash-brickers/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'hideakiyamada', // Usually your GitHub org/user name.
+  projectName: 'smash-brickers', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
